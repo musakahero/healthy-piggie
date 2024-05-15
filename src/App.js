2f224876b7d2be14bf0e4191ddfec94378da68a5
Theodor <teo.ruschev@gmail.com>
2024-05-15 21:17:49 +0300

    Configured Footer CSS
    Stuck footer to the bottom of the page.

--- src/App.js
+++ src/App.js
@@ -7,32 +7,31 @@ import { Home } from './components/Home/Home';
 import { Combos } from './components/Combos/Combos';
 import { Footer } from './components/Footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faWheatAwnCircleExclamation} from '@fortawesome/free-solid-svg-icons';
+import { faWheatAwnCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className={styles["app"]}>
       <header className={styles["app-header"]}>
-        <Navigation logoIcon = {<FontAwesomeIcon icon={faWheatAwnCircleExclamation}/>}/>
+        <Navigation logoIcon={<FontAwesomeIcon icon={faWheatAwnCircleExclamation} />} />
       </header>
-        <main className={styles['app-main']}>
-          <Routes>
+      <main className={styles['app-main']}>
+        <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/pop-edible" element={<PopEdible />}></Route>
           <Route path="/pop-toxic" element={<PopToxic />}></Route>
           <Route path="/combos" element={<Combos />}></Route>
-          {/* <Route path="/contacts" element={<Contacts />}></Route> */}
-            {/* <Route path="/terms" element={<Terms />}></Route> */}
-            {/* <Route path="/privacy" element={<Privacy />}></Route> */}
-            {/* <Route path="/cookies" element={<Cookies />}></Route> */}
-            {/* <Route path="/sources" element={<Sources />}></Route> */}
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+/* <Route path="/contacts" element={<Contacts />}></Route> */
+/* <Route path="/terms" element={<Terms />}></Route> */
+/* <Route path="/privacy" element={<Privacy />}></Route> */
+/* <Route path="/cookies" element={<Cookies />}></Route> */
+/* <Route path="/sources" element={<Sources />}></Route> */
